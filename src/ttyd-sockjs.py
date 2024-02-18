6d8fd38b7e7bce74934009decde80ac65cf9c456
HAYAMI <noname@example.com>
2024-02-18 23:56:28 +0900

    Various improvements

--- src/ttyd-sockjs.py
+++ src/ttyd-sockjs.py
@@ -2,51 +2,93 @@
 #import logging
 import os
 
-from aiohttp import web
+from aiohttp import web, WSMsgType
 import sockjs
 
 
-CONSOLE_HTML = open(os.path.join(os.path.dirname(__file__), '../ttyd/ttyd-1.7.2/html/dist/inline.html'), 'rb').read()
+TOPPAGE_HTML = open(os.path.join(os.path.dirname(__file__), '../ttyd/ttyd-1.7.2/html/dist/inline.html'), 'rb').read()
 
+NOPATCH_HTML = open(os.path.join(os.path.dirname(__file__), 'nopatch.html'), 'rb').read()
 
-async def token_handler(msg, session):
-    if session.manager is None:
-        return
+class TtydServer:
+    def __init__(self, use_sockjs=True):
+        self.use_sockjs = use_sockjs
 
-    match msg.type:
-        case sockjs.MSG_OPEN:
-            print("MSG_OPEN\n")
+    async def toppage_handler(self, request):
+        if self.use_sockjs:
+            return web.Response(body=TOPPAGE_HTML, content_type='text/html')
+        else:
+            return web.Response(body=NOPATCH_HTML, content_type='text/html')
 
-        case sockjs.MSG_MESSAGE:
-            print("MSG_MESSAGE: %s\n" % msg.data)
-            session.send("1/bin/sh (ttyd-MIKADO)")
-            print("sent: 1/bin/sh...\n")
+    async def token_handler(self, request):
+        token = {'token': 'abc'}
+        return web.json_response(token)
 
-        case sockjs.MSG_CLOSE:
-            print("MSG_CLOSE\n")
+    async def websocket_handler(self, request):
+        ws = web.WebSocketResponse()
+        await ws.prepare(request)
+        print('WebSocket: connection ready')
 
-        case sockjs.MSG_CLOSED:
-            print("MSG_CLOSED\n")
+        async for msg in ws:
+            match msg.type:
+                 case WSMsgType.BINARY:
+                     print('WSMsgType.BINARY: ', end='')
+                     await self._chat(msg.data, ws.send_bytes, True);
+                 case _:
+                     print('unexpected msg.type: 0x%x (WSMsgTye)' % msg.type)
 
-        case _:
-            print("unexpected msg.type\n")
+        await ws.close()
+        print('WebSoocket: connection closed')
+        return ws
 
+    async def sockjs_handler(self, msg, session):
+        if session.manager is None:
+            return
 
-def console(request):
-    return web.Response(body=CONSOLE_HTML, content_type='text/html')
+        match msg.type:
+            case sockjs.MSG_OPEN:
+                print('SockJS: connection ready')
+            case sockjs.MSG_MESSAGE:
+                print('sockjs.MSG_MESSAGE: ', end='')
+                await self._chat(msg.data, session.send, False)
+            case sockjs.MSG_CLOSE:
+                print('SockJS: connection closing')
+            case sockjs.MSG_CLOSED:
+                print('SockJS: connection closed')
+            case _:
+                 print('unexpected msg.type: %d (sockjs)' % msg.type)
 
+    async def _chat_send(self, data, sendfunc, bin):
+        if bin:
+            await sendfunc(data.encode('ascii'))
+        else:
+            # XXX await XXX
+            sendfunc(data)
 
-def token(request):
-    return web.Response(body='{"token": "abc"}', content_type='application/json', charset='utf-8')
+    async def _chat(self, data, sendfunc, isBytes):
+        if isBytes:
+            data = data.decode()
+
+        print(data)
+        if data[0] == '{':
+            await self._chat_send('1/bin/sh (ttyd-MIKADO)', sendfunc, isBytes)
+            await self._chat_send('2{}', sendfunc, isBytes)
+            await self._chat_send('0prompt$ ', sendfunc, isBytes)
+        else:
+            await self._chat_send('0{}'.format(data[1:]), sendfunc, isBytes)
 
 
 if __name__ == '__main__':
     #logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 
+    ttyd = TtydServer(True)
     app = web.Application()
-    app.router.add_route('GET', '/', console)
-    app.router.add_route('GET', '/token', token)
-    sockjs.add_endpoint(app, token_handler, name='console')
+    app.add_routes([web.get('/', ttyd.toppage_handler),
+                    web.get('/token', ttyd.token_handler)])
+    if ttyd.use_sockjs:
+        sockjs.add_endpoint(app, ttyd.sockjs_handler, name='ttyd', prefix='/sockjs')
+    else:
+        app.add_routes([web.get('/ws', ttyd.websocket_handler)])
 
     web.run_app(app, host='localhost', port=8000)
     # https://docs.aiohttp.org/en/stable/web_reference.html#utilities
