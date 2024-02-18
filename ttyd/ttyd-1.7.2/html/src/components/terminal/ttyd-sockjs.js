5e8771154c6fa3c5f2f460f0bbddda15a2e521d3
HAYAMI <noname@example.com>
2024-02-18 22:23:33 +0900

    Add onmessage_hook()

--- ttyd/ttyd-1.7.2/html/src/components/terminal/ttyd-sockjs.js
+++ ttyd/ttyd-1.7.2/html/src/components/terminal/ttyd-sockjs.js
@@ -3,12 +3,33 @@
 const SockJS = require('sockjs-client');
 
 function TtydSockJS(url, protocols) {
+    this.textEncoder = new TextEncoder();
+    this.textDecoder = new TextDecoder();
+
+    this.onmessage_orig = function(event) {};
+    this.onmessage_hook = function(event) {
+        if (typeof(event.data) === 'string') {
+            event.data = this.textEncoder.encode(event.data);
+        } else {
+            const fname = event.data.constructor.name;
+            console.log('[ttyd-sockjs] onmessage(): unexpected object type: ' + fname);
+        }
+        return this.onmessage_orig(event);
+    };
+    Object.defineProperty(this, "onmessage", {
+        get: function() {
+            return this.onmessage_hook;
+        },
+        set: function(func) {
+            this.onmessage_orig = func;
+        }
+    });
+
     const options = {};	// e.g. { transports: ["xhr-streaming", "xhr-polling"] }
     const pstr = JSON.stringify(protocols);
     const ostr = JSON.stringify(options);
     console.log(`[ttyd-sockjs] SockJS.call(this, ${url}, ${pstr}, ${ostr})`);
     SockJS.call(this, url, protocols, options);
-    this.textDecoder = new TextDecoder();
 }
 
 TtydSockJS.prototype = Object.create(SockJS.prototype);
