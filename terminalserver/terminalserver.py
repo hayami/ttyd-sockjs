0efb335574c0e3c4870e7755676a11f17072f88e
HAYAMI <noname@example.com>
2025-01-05 12:09:47 +0900

    none

--- terminalserver/terminalserver.py
+++ terminalserver/terminalserver.py
@@ -15,9 +15,9 @@ class TerminalServer:
 
     async def toppage_handler(self, request):
         if self.use_sockjs:
-            return web.FileResponse('static/terminal.html')
+            return web.FileResponse('main.html')
         else:
-            return web.FileResponse('static/terminal.html')
+            return web.FileResponse('main.html')
 
     async def token_handler(self, request):
         token = {'token': 'abc'}
